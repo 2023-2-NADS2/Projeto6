4678d0fc4c819897fd57cb265991048e80ccbcb6
Kevin Shiroma <90286842+KevinShiroma@users.noreply.github.com>
2023-11-16 12:42:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const cookieBox = document.querySelector(".wrapper"),
   buttons = document.querySelectorAll(".button");
 
 const executeCodes = () => {
-  //if cookie contains codinglab it will be returned and below of this code will not run
+  
   if (document.cookie.includes("codinglab")) return;
   cookieBox.classList.add("show");
 
@@ -19,14 +19,14 @@ const executeCodes = () => {
     button.addEventListener("click", () => {
       cookieBox.classList.remove("show");
 
-      //if button has acceptBtn id
+      
       if (button.id == "acceptBtn") {
-        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+        // set do tempo de cookies
         document.cookie = "cookieBy= codinglab; max-age=" + 60 * 60 * 24 * 30;
       }
     });
   });
 };
 
-//executeCodes function will be called on webpage load
+//adiciona evento ao carregar a pagina
 window.addEventListener("load", executeCodes);
